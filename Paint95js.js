d557bbf666bb98eb29b809f5983dcc8b14bea36e
Daniel Weissberger <danielweissberger@gmail.com>
2016-12-25 00:47:59 +0200

    Added rotate canvas (bug with painting on rotated canvas to be fixed). Updated saving functions. Removed comments.

--- Paint95js.js
+++ Paint95js.js
@@ -1,6 +1,6 @@
 
 
-var selectedColor = "white";
+var selectedColor = "";
 var brushSize = 4;
 var brushRadius = 0;
 var canvasSize = 500;
@@ -8,7 +8,7 @@ var toolbar = document.getElementById("toolbar");
 var toolbar1 = document.getElementById("toolbar1");
 var toolbar2 = document.getElementById("toolbar2");
 var canvas = document.getElementById("canvas");
-//
+
 function toolBarDivMaker(item, toolbarStrip, height){
     height = height || 60;
     var tbDiv = document.createElement("div");
@@ -16,7 +16,6 @@ function toolBarDivMaker(item, toolbarStrip, height){
     tbDiv.style.width = "100%";
     tbDiv.style.display = "inline-block";
     tbDiv.style.textAlign = "center";
-//            tbDiv.style.border = "2px solid black";
     tbDiv.style.veticalAlign = "middle";
 
     if(!(item.constructor === Array)) {
@@ -106,6 +105,7 @@ var brushOptionsContainer = document.createElement("div");
 brushOptionsContainer.style.display = "inline-block";
 brushOptionsContainer.style.width = "100px";
 brushOptionsContainer.id = "brushOptions";
+
 var brushOptionsText = document.createElement("p");
 brushOptionsText.textContent = "Brush Size";
 brushOptionsText.style.color = "blue";
@@ -136,13 +136,6 @@ brushOptions.addEventListener("change",setBrushSize);
 brushOptionsContainer.appendChild(brushOptions);
 
 toolBarDivMaker(brushOptionsContainer,toolbar2,50);
-//    toolbar2.appendChild(brushOptionsContainer);
-
-//
-//
-//
-//
-
 
 
 function canvasLarger(){
@@ -170,8 +163,7 @@ cSmaller.classList.add("options");
 cSmaller.addEventListener("click",canvasSmaller);
 
 toolBarDivMaker([cSmaller,cLarger],toolbar2,40)
-//    toolbar2.appendChild(cSmaller);
-//    toolbar2.appendChild(cLarger);
+
 
 function setRadius(){
     if (brushRadius == 0) {
@@ -184,22 +176,6 @@ function setRadius(){
 
 }
 
-function createBrushButton(size, radius, divText){
-
-    var btn = document.createElement("button");
-    btn.style.height = size + "px";
-    btn.style.width = size + "px";
-    btn.textContent = divText;
-    btn.style.backgroundColor = "lightgrey";
-    btn.style.color = "blue";
-    btn.style.borderRadius = radius + "px";
-    btn.style.display = "inline-block";
-    btn.style.margin = "3px";
-    btn.style.border = "2px solid black";
-    btn.addEventListener("click", setRadius );
-    toolBarDivMaker(btn,toolbar2,75);
-//        toolbar2.appendChild(btn);
-}
 
 
 function clearCanvas(){
@@ -212,15 +188,41 @@ function clearCanvas(){
 
 
 var clrCanvas = document.createElement("BUTTON");
-clrCanvas.textContent = "CLEAR CANVAS";
+clrCanvas.textContent = "Clear Canvas";
 clrCanvas.backgroundColor = "grey";
 clrCanvas.style.color = "blue";
 clrCanvas.classList.add("options");
 clrCanvas.addEventListener("click",clearCanvas)
-toolBarDivMaker(clrCanvas,toolbar2,60);
+toolBarDivMaker(clrCanvas,toolbar2,40);
 
-createBrushButton(60,30,"Circle Brush");
-createBrushButton(60,0,"Square Brush");
+
+function updateBrushRadius(e){
+    console.log(e.target.value);
+    var radius = e.target.value;
+    var brush = document.getElementById("brush");
+    brush.style.borderRadius = radius + "px";
+    brushRadius = radius;
+}
+
+var brushSlider = document.createElement("input");
+brushSlider.type = "range";
+brushSlider.min = 0;
+brushSlider.max = 25;
+brushSlider.addEventListener("input",updateBrushRadius);
+var brush = document.createElement("div");
+brush.style.height = 60 + "px";
+brush.style.width = 60 + "px";
+brush.textContent = "Brush";
+brush.id = "brush";
+brush.style.backgroundColor = "lightgrey";
+brush.style.color = "blue";
+brush.style.borderRadius = 0 + "px";
+brush.style.display = "inline-block";
+brush.style.lineHeight = "60px";
+brush.addEventListener("click", setRadius );
+toolBarDivMaker(brush,toolbar2,75);
+toolBarDivMaker(brushSlider,toolbar2,30);
+
 
 
 function updateRGBPreview(e){
@@ -229,6 +231,9 @@ function updateRGBPreview(e){
     var B = document.getElementById("rgbB").value;
     var rgbDiv = document.getElementById("rgbDiv");
     rgbDiv.style.backgroundColor = "rgb("+R+","+G+","+B+")";
+    if(rgbDiv.classList.contains("selected")){
+        selectedColor = "rgb("+R+","+G+","+B+")";
+    }
 }
 
 
@@ -238,13 +243,13 @@ rgbDivTitle.style.color = "blue";
 var rgbDiv = document.createElement("div");
 rgbDiv.style.display = "inline-block";
 
+
 rgbDiv.style.height = "50px";
 rgbDiv.style.width = "50px";
 rgbDiv.id = "rgbDiv";
 rgbDiv.className = "unselected";
-rgbDiv.style.backgroundColor = "white";
+rgbDiv.style.backgroundColor = "rgb(0,0,0)";
 rgbDiv.addEventListener("click",colorSelect);
-
 
 var R = document.createElement("p");
 R.style.display = "inline-block";
@@ -262,17 +267,26 @@ B.style.color = "Blue";
 var rgbR = document.createElement("input");
 rgbR.type = "number";
 rgbR.id = "rgbR";
-rgbR.addEventListener("change", updateRGBPreview);
+rgbR.value = 0;
+rgbR.min = 0;
+rgbR.max = 255;
+rgbR.addEventListener("input", updateRGBPreview);
 rgbR.classList.add("rgb");
 var rgbG = document.createElement("input");
 rgbG.type = "number";
 rgbG.id = "rgbG";
-rgbG.addEventListener("change", updateRGBPreview);
+rgbG.value = 0;
+rgbG.min = 0;
+rgbG.max = 255;
+rgbG.addEventListener("input", updateRGBPreview);
 rgbG.classList.add("rgb");
 var rgbB = document.createElement("input");
 rgbB.type = "number";
 rgbB.id = "rgbB";
-rgbB.addEventListener("change", updateRGBPreview);
+rgbB.value = 0;
+rgbB.min= 0;
+rgbB.max = 255;
+rgbB.addEventListener("input", updateRGBPreview);
 rgbB.classList.add("rgb");
 
 
@@ -286,74 +300,75 @@ toolBarDivMaker(rgbDiv,toolbar2,60);
 
 var savedFiles="";
 function saveCanvas(e){
-    var canvas = document.getElementById("canvas");
+    var canvasDiv = document.getElementById("canvasDiv");
     var saveString = prompt("Enter the name for the current canvas to be saved:")
     var files = localStorage.getItem("savedFiles");
     if (files === null){
-        localStorage.setItem("savedFiles","");
+        localStorage.setItem("savedFiles","[]");
     }
-    if (localStorage.getItem("savedFiles").length>1) {
-        localStorage.setItem("savedFiles", files +saveString + ",");
-        localStorage.setItem(saveString, canvas.innerHTML);
-    }
-    else{
-        localStorage.setItem("savedFiles",saveString+",")
-        localStorage.setItem(saveString,canvas.innerHTML);
-    }
+    var files = JSON.parse(localStorage.getItem("savedFiles"));
+    files.push(saveString);
+    localStorage.setItem("savedFiles", JSON.stringify(files));
+    localStorage.setItem(saveString, canvasDiv.innerHTML);
+
+
 
 
 }
 function loadCanvas(e){
-    var canvas = document.getElementById("canvas");
-    canvas.innerHTML = localStorage.getItem(e.target.value);
+    var canvasDiv = document.getElementById("canvasDiv");
+    canvasDiv.innerHTML = localStorage.getItem(e.target.value);
+    canvas = document.getElementById("canvas");
+    canvas.addEventListener("mousemove",paintDiv);
     var loadOptions = document.getElementById("loadOptions");
     loadOptions.style.display = "none";
     var blankOption= document.getElementById("blankOption");
     blankOption.selected = true;
 }
-//<option value="" disabled selected style="display:none;">Label</option>
-var optionsLoaded = false;
+
 function loadCanvasGUI()
 {
-    if (!optionsLoaded) {
-        var savedFiles = localStorage.getItem("savedFiles").split(",")
-        var optionsToCreate = savedFiles.length - 1;
-        var loadOptions = document.createElement("select");
-        loadOptions.id = "loadOptions";
-        var blankOption = document.createElement("option");
-        blankOption.disabled = true;
-        blankOption.selected = true;
-        blankOption.id = "blankOption";
-        blankOption.textContent = "Select A File";
-        loadOptions.add(blankOption);
-        for (var i = 0; i < optionsToCreate; i++) {
-            var newOption = document.createElement("option");
-            newOption.textContent = savedFiles[i];
-            loadOptions.add(newOption);
-        }
-        loadOptions.addEventListener("change", loadCanvas);
-        toolBarDivMaker(loadOptions, toolbar2);
-        optionsLoaded = true;
-    }
-    else{
-        loadOptions = document.getElementById("loadOptions");
-        loadOptions.style.display = "initial";
-        var length = loadOptions.options.length;
-        console.log(loadOptions.options.length);
+        var savedFiles = JSON.parse(localStorage.getItem("savedFiles"));
+        var optionsToCreate = savedFiles.length;
+        var loadOptions = document.getElementById("loadOptions");
         while (loadOptions.options.length>1) {
             loadOptions.removeChild(loadOptions.lastChild);
         }
-        var savedFiles = localStorage.getItem("savedFiles").split(",")
-        var optionsToCreate = savedFiles.length-1;
-        console.log("Options To Create: " + optionsToCreate);
         for (var i = 0; i < optionsToCreate; i++) {
             var newOption = document.createElement("option");
             newOption.textContent = savedFiles[i];
             loadOptions.add(newOption);
         }
-    }
+        loadOptions.style.display = "inline-block";
 
 }
+
+var rotated = false;
+function rotateCanvas(e)
+{
+    var deg;
+    if(rotated){
+        deg = 0;
+    }
+    else {
+        deg = 90;
+    }
+    var canvas = document.getElementById("canvas");
+    canvas.style.webkitTransform = 'rotate('+deg+'deg)';
+    canvas.style.mozTransform    = 'rotate('+deg+'deg)';
+    canvas.style.msTransform     = 'rotate('+deg+'deg)';
+    canvas.style.oTransform      = 'rotate('+deg+'deg)';
+    canvas.style.transform       = 'rotate('+deg+'deg)';
+    rotated = !rotated;
+}
+
+
+var rotateBtn = document.createElement("button");
+rotateBtn.textContent = "Rotate Canvas";
+rotateBtn.addEventListener("click",rotateCanvas);
+rotateBtn.style.color = "blue";
+rotateBtn.style.fontSize = "13px";
+toolBarDivMaker(rotateBtn,toolbar2,50);
 
 
 var saveBtn = document.createElement("button");
@@ -368,32 +383,49 @@ loadBtn.textContent = "Load Canvas";
 loadBtn.addEventListener("click", loadCanvasGUI);
 loadBtn.style.color = "blue";
 
+var loadOptions = document.createElement("select");
+loadOptions.id = "loadOptions";
+loadOptions.style.display = "none";
+loadOptions.addEventListener("change", loadCanvas);
+var blankOption = document.createElement("option");
+blankOption.disabled = true;
+blankOption.selected = true;
+blankOption.id = "blankOption";
+blankOption.textContent = "Select A File";
+loadOptions.add(blankOption);
+loadOptions.style.display = "none";
+
 toolBarDivMaker(loadBtn,toolbar2,30);
+toolBarDivMaker(loadOptions,toolbar2);
+
 
 toolbar.appendChild(toolbar2);
 
 
 function paintDiv(e){
 
-//        if(e.which ==1 && document.getElementsByClassName("selected").length>0) {
-    if(e.which == 1){
-        canvas = document.getElementById("canvas");
-        var newDiv = document.createElement("div");
-        newDiv.style.backgroundColor = selectedColor;
-        newDiv.style.top = (e.pageY-canvas.offsetTop) + "px";
-        newDiv.style.left = (e.clientX-canvas.offsetLeft) + "px";
-        newDiv.style.height = brushSize + "px";
-        newDiv.style.width = brushSize + "px";
-        newDiv.style.borderRadius = brushRadius + "px";
-        newDiv.style.position = "absolute";
-        newDiv.style.display = "inline-block";
-        canvas.appendChild(newDiv);
-        return;
+
+    if(e.which == 1)
+    {
+            if(selectedColor != "") {
+                canvas = document.getElementById("canvas");
+                var newDiv = document.createElement("div");
+                newDiv.style.backgroundColor = selectedColor;
+                newDiv.style.left = (e.pageX - canvas.offsetLeft) + "px";
+                newDiv.style.top = (e.pageY - canvas.offsetTop) + "px";
+                newDiv.style.height = brushSize + "px";
+                newDiv.style.width = brushSize + "px";
+                newDiv.style.borderRadius = brushRadius + "px";
+                newDiv.style.position = "absolute";
+                newDiv.style.display = "inline-block";
+                canvas.appendChild(newDiv);
+            }
+            else{
+                alert("Please select a color, you can select the RGB Preview as well as any color on the left palette")
+            }
     }
 
 }
-
-
 
 
 canvas.style.height = canvasSize + "px";
